8134ef8f796f35b41bbac5f3e61653ce4346130f
Joel John <joeljohn@joels-mbp.lan>
2025-03-04 12:49:31 -0500

    initial commit

---
+++ src/main/java/joel/john/pokemonhub/PokemonhubApplication.java
@@ -0,0 +1,13 @@
+package joel.john.pokemonhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemonhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokemonhubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/joel/john/pokemonhub/PokemonhubApplicationTests.java
@@ -0,0 +1,13 @@
+package joel.john.pokemonhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
